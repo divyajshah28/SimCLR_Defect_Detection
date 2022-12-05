5facef68d8b86119ea066da3ddb552db4d1ef33e
divyajshah28 <68955544+divyajshah28@users.noreply.github.com>
2022-12-06 00:00:59 +0100

    Update Contrastive_learning.py

--- Contrastive_learning.py
+++ Contrastive_learning.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat Nov 26 20:33:32 2022
+
+@author: Divyaj
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sun Nov 13 15:43:37 2022
 
 @author: Divyaj
@@ -9,8 +16,9 @@ import tensorflow.compat.v1 as tf
 tf.compat.v1.enable_eager_execution()
 tf.config.experimental_run_functions_eagerly(True)
 
+from constants import *
 import sys, os
-sys.path.append('D:\CS230_Deep_Learning\Projects\SimCLR_Defect_Detection')
+sys.path.append(wdir)
 import re
 import numpy as np
 import math
@@ -25,8 +33,8 @@ from keras.utils import image_dataset_from_directory
 from tensorflow import keras
 from tensorflow.keras import layers
 
-from constants import *
 import pathlib
+import random
 
 data_labelled = pathlib.Path(labelled_data_path)
 data_unlabelled = pathlib.Path(unlabelled_data_path)
@@ -44,69 +52,94 @@ Code credits: https://keras.io/examples/vision/semisupervised_simclr/
 
 """
 
-#helper function for loading the data
-def get_data(data_labelled, data_unlabelled, batch_size, IMG_SIZE):
-    #loading labelled and unlabelled dataset together
-    
-    img_height, img_width = IMG_SIZE
-    batch_size= batch_size
-    unlabelled_train_ds = image_dataset_from_directory(
-        data_unlabelled, 
-        label_mode = None,
-        seed=123,
-        image_size = (img_height, img_width),
-        batch_size = batch_size)
-    
-    labelled_train_ds = image_dataset_from_directory(
-      data_labelled,
-      validation_split=0.4,
-      subset="training",
-      seed=123,
-      image_size=(img_height, img_width),
-      batch_size=batch_size)
-    
-    val_ds = image_dataset_from_directory(
-      data_labelled,
-      validation_split=0.4,
-      subset="validation",
-      seed=123,
-      image_size=(img_height, img_width),
-      batch_size=batch_size)
+def prepare_dataset():
+    # Labeled and unlabeled samples are loaded synchronously
+    # with batch sizes selected accordingly
+    steps_per_epoch = (unlabeled_dataset_size + labeled_dataset_size) // batch_size
+    unlabeled_batch_size = unlabeled_dataset_size // steps_per_epoch
+    labeled_batch_size = labeled_dataset_size // steps_per_epoch
+    print(
+        f"batch size is {unlabeled_batch_size} (unlabeled) + {labeled_batch_size} (labeled)"
+    )
 
-    """
-    As the original dataset doesn't contain a test set, you will create one. 
-    To do so, determine how many batches of data are available in the 
-    validation set using tf.data.experimental.cardinality, 
-    then move 20% of them to a test set.
-    """
-
-    val_batches = tf.data.experimental.cardinality(val_ds)
-    test_ds = val_ds.take(val_batches // 5)
-    val_ds = val_ds.skip(val_batches // 5)
-
-    """
-    Use buffered prefetching to load images from disk without having 
-    I/O become blocking
-    """
-    AUTOTUNE = tf.data.AUTOTUNE
-
-    #labelled_train_ds = labelled_train_ds.prefetch(buffer_size=AUTOTUNE)
-    #unlabelled_train_ds = unlabelled_train_ds.prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
-    test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
+    unlabeled_train_dataset = (
+        tfds.load("stl10", split="unlabelled", as_supervised=True, shuffle_files=True)
+        .shuffle(buffer_size=10 * unlabeled_batch_size)
+        .batch(unlabeled_batch_size)
+    )
+    labeled_train_dataset = (
+        tfds.load("stl10", split="train", as_supervised=True, shuffle_files=True)
+        .shuffle(buffer_size=10 * labeled_batch_size)
+        .batch(labeled_batch_size)
+    )
+    test_dataset = (
+        tfds.load("stl10", split="test", as_supervised=True)
+        .batch(batch_size)
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+    )
 
     # Labeled and unlabeled datasets are zipped together
-    train_ds = tf.data.Dataset.zip(
-        (unlabelled_train_ds, labelled_train_ds)
+    train_dataset = tf.data.Dataset.zip(
+        (unlabeled_train_dataset, labeled_train_dataset)
     ).prefetch(buffer_size=tf.data.AUTOTUNE)
 
-    return train_ds, labelled_train_ds, test_ds
+    return train_dataset, labeled_train_dataset, test_dataset
 
 
-train_dataset, labelled_train_dataset, test_dataset = get_data(data_labelled,
-                                                               data_unlabelled, 
-                                                               batch_size, IMG_SIZE)
+# Load MVtec_AD dataset
+train_dataset, labeled_train_dataset, test_dataset = prepare_dataset()
 
+class Cut_Paste(layers.Layer):
+    def __init__(self, x_scale = 10, y_scale = 10, IMG_SIZE = (224,224), **kwargs):
+        super().__init__(**kwargs)
+        
+        """
+        defining the x span and the y span of the box to cutout
+        x_scale and y_scale are taken as inputs as % of the width and height of the image
+        size
+        """        
+        self.size_x, self.size_y = IMG_SIZE
+        self.span_x = int(x_scale*self.size_x*0.01)
+        self.span_y = int(y_scale*self.size_y*0.01)
+        
+    #getting the vertices for cut and paste    
+    def get_vertices(self):
+        
+        #determining random points for cut and paste 
+        """ since the images in the dataset have the object of interest in the center of 
+        the Image, the cutout will be taken from the central 25% of the image"""
+        fraction = 0.25
+        vert_x = random.randint(int(self.size_x*0.5*(1-fraction)),
+                               int(self.size_x*0.5*(1+fraction)))        
+        vert_y = random.randint(int(self.size_y*0.5*(1-fraction)),
+                               int(self.size_y*0.5*(1+fraction)))
+        
+        start_x = int(vert_x-self.span_x/2)
+        start_y = int(vert_y-self.span_y/2)
+        end_x = int(vert_x+self.span_x/2)
+        end_y = int(vert_y+self.span_y/2)
+        
+        return start_x, start_y, end_x, end_y
+        
+    def call(self, image):
+        image = tf.Variable(image).numpy()
+        #getting random vertices for cutting
+        cut_start_x, cut_start_y, cut_end_x, cut_end_y = self.get_vertices()
+        
+        #getting the image as a sub-image
+        #image = tf.Variable(image)
+        sub_image = image[cut_start_x:cut_end_x,cut_start_y:cut_end_y,:]
+        
+        #getting random vertices for pasting
+        paste_start_x, paste_start_y, paste_end_x, paste_end_y = self.get_vertices()
+        
+        #replacing a part of the image at random location with sub_image
+        image[paste_start_x:paste_end_x,
+              paste_start_y:paste_end_y,:] = sub_image
+        
+        return tf.convert_to_tensor(image)
+ 
+    
 def get_encoder():
     base_model = keras.applications.resnet50.ResNet50(
         input_shape = INPUT_SHAPE,
@@ -131,21 +164,39 @@ def get_augmenter(min_area, brightness, jitter):
         ]
     )
 
+"""
+Our model calss inherits from the built in class called keras.Model. 
+Our class is the sub-class and the keras.Model is the super class
+-- also called as the parent class or the base class.
 
+This way of creating a class from another class is also called as sub-classing
+or inheritance"""
 
 class Contrastive_learning_model(keras.Model):
+    
+    
+    """ This article describes why self is explicitly defined in python 
+    and why it is necessary. It basically helps us to create multiple objects
+    by creating multiple instances. Every time a new object is called, self
+    will create a new instance for that class
+    
+    Source: https://www.programiz.com/article/python-self-why#:~:text=If%20there%20was%20no%20self,its%20own%20attributes%20and%20methods.
+    """
+    
     def __init__(self):
         
-        """
+        '''
         super is used here since our defined class inherits from the class
         keras.Model. We don't have to rewrite the functions in keras.model again
         and super can diretly call it for us. This is called as supercharging a
-        class"""
+        class
+        '''
         super().__init__()
         
         self.temperature=temperature
         self.contrastive_augmenter = get_augmenter(**contrastive_augmentation)
         self.classification_augmenter = get_augmenter(**classification_augmentation)
+        self.cut_paste = Cut_Paste(**cut_paste_augmentation)
         self.encoder = get_encoder()
         
         #Non-linear MLP as projection head
@@ -156,7 +207,8 @@ class Contrastive_learning_model(keras.Model):
         
         # Single dense layer for linear probing
         self.linear_probe = keras.Sequential(
-            [layers.Input(shape=(None,1,2048)), layers.Dense(10)], name="linear_probe")
+            [layers.Input(shape=(None,1,2048)), 
+             layers.Dense(10)], name="linear_probe")
             
         self.encoder.summary()
         self.projection_head.summary()
@@ -186,21 +238,29 @@ class Contrastive_learning_model(keras.Model):
             self.probe_loss_tracker,
             self.probe_accuracy]
                
+    def cross_entropy(self, similarities):
+        # similarities is a matrix of shape N*N where N is the batch size
+        self.exp_n = np.exp(similarities)
+        loss =  np.sum(np.divide(np.diagonal(self.exp_n), 
+                                    np.sum(self.exp_n, axis=1)-np.diagonal(self.exp_n)))
+    
+        return tf.convert_to_tensor(loss)
+    
     def contrastive_loss(self, projections_1, projections_2):
         # InfoNCE loss (information noise-contrastive estimation)
         # NT-Xent loss (normalized temperature-scaled cross entropy)
-    
+        
         # Cosine similarity: the dot product of the l2-normalized feature vectors
         projections_1 = tf.math.l2_normalize(projections_1, axis=1)
         projections_2 = tf.math.l2_normalize(projections_2, axis=1)
         similarities = (
             tf.matmul(projections_1, projections_2, transpose_b=True) / self.temperature
         )
-    
         # The similarity between the representations of two augmented views of the
         # same image should be higher than their similarity with other views
         batch_size = tf.shape(projections_1)[0]
         contrastive_labels = tf.range(batch_size)
+        
         self.contrastive_accuracy.update_state(contrastive_labels, similarities)
         self.contrastive_accuracy.update_state(
             contrastive_labels, tf.transpose(similarities)
@@ -208,14 +268,19 @@ class Contrastive_learning_model(keras.Model):
     
         # The temperature-scaled similarities are used as logits for cross-entropy
         # a symmetrized version of the loss is used here
+        
         loss_1_2 = keras.losses.sparse_categorical_crossentropy(
             contrastive_labels, similarities, from_logits=True
         )
         loss_2_1 = keras.losses.sparse_categorical_crossentropy(
             contrastive_labels, tf.transpose(similarities), from_logits=True
         )
+     
         return (loss_1_2 + loss_2_1) / 2       
             
+    """the below function called train_step is called by the Model.fit method
+    and the batch size and epoch iteration is taken care of internally"""
+    
     def train_step(self, data):
         (unlabeled_images), (labeled_images, labels) = data
         print(unlabeled_images.shape, labeled_images.shape)
@@ -230,6 +295,7 @@ class Contrastive_learning_model(keras.Model):
             # The representations are passed through a projection mlp
             projections_1 = self.projection_head(features_1, training=True)
             projections_2 = self.projection_head(features_2, training=True)
+          
             contrastive_loss = self.contrastive_loss(projections_1, projections_2)
         gradients = tape.gradient(
             contrastive_loss,
@@ -283,15 +349,37 @@ class Contrastive_learning_model(keras.Model):
 pretraining_model = Contrastive_learning_model()
 pretraining_model.compile(
     contrastive_optimizer=keras.optimizers.Adam(),
-    probe_optimizer=keras.optimizers.Adam(),
+    probe_optimizer=keras.optimizers.Adam(),run_eagerly=True
 )
 
+""" the fit function of the keras.Model will call the train_step method.
+If the train_step method is not defined, it will pick the method from the
+parent calss. If defined, it will take the function from the sub-class 
+since the object is defined for the sub-class"""
+
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
 pretraining_history = pretraining_model.fit(
-    train_dataset, epochs=num_epochs, validation_data=test_dataset
+    train_dataset, epochs=num_epochs, validation_data=test_dataset,
+    callbacks=[cp_callback]
 )
+
+!mkdir -p saved_model
+pretraining_model.save('saved_model/pretraining_model')
+
 print(
     "Maximal validation accuracy: {:.2f}%".format(
         max(pretraining_history.history["val_p_acc"]) * 100
     )
 )
+
+loss, acc = pretraining_model.evaluate(test_dataset, verbose=2)
+print("Pretraining model, accuracy: {:5.2f}%".format(100 * acc))
 
