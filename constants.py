4465c2e10efa0685f5901341f882bf0cc35efdb2
divyajshah28 <68955544+divyajshah28@users.noreply.github.com>
2022-12-06 00:01:42 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,14 +5,18 @@ Created on Sat Nov 12 23:46:27 2022
 @author: Divyaj
 """
 
-labelled_data_path = "D:\CS230_Deep_Learning\\CNN_Dataset_defect_detection\\labelled"
-unlabelled_data_path = "D:\CS230_Deep_Learning\CNN_Dataset_defect_detection\\unlabelled"
+wdir="D:\CS230_Deep_Learning\Projects\SimCLR_Defect_Detection"
 IMG_SIZE = (224,224)
 INPUT_SHAPE = IMG_SIZE + (3,)
-batch_size = 1
-num_epochs = 10
+# Algorithm hyperparameters
+batch_size = 2
+num_epochs = 1
 temperature = 0.1
 width = 128
 # Stronger augmentations for contrastive, weaker ones for supervised training
 contrastive_augmentation = {"min_area": 0.25, "brightness": 0.6, "jitter": 0.2}
 classification_augmentation = {"min_area": 0.75, "brightness": 0.3, "jitter": 0.1}
+cut_paste_augmentation = {"x_scale":10, "y_scale":10, "IMG_SIZE":IMG_SIZE}
+# Dataset hyperparameters
+unlabeled_dataset_size = 1000
+labeled_dataset_size = 50
