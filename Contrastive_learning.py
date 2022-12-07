32bd3000a42d98ac954d6ead491f779141880811
divyajshah28 <68955544+divyajshah28@users.noreply.github.com>
2022-12-07 23:13:29 +0100

    Update Contrastive_learning.py

--- Contrastive_learning.py
+++ Contrastive_learning.py
@@ -18,7 +18,7 @@ tf.config.experimental_run_functions_eagerly(True)
 
 from constants import *
 import sys, os
-sys.path.append(wdir)
+#sys.path.append(wdir)
 import re
 import numpy as np
 import math
@@ -28,11 +28,11 @@ import tensorflow_datasets as tfds
 
 import matplotlib
 import matplotlib.pyplot as plt
-from data_processing import preprocess_image
+#from data_processing import preprocess_image
 from keras.utils import image_dataset_from_directory
 from tensorflow import keras
 from tensorflow.keras import layers
-from movtec_ad import mvtec_ad
+from mvtec_ad import mvtec_ad
 
 import pathlib
 import random
